cf2117bb9eba56b4652b2d003428bf1accd3dd23
TerronUA <kucher.taras@gmail.com>
2016-02-09 15:16:48 +0200

    GravityManager and LevelBlock

---
+++ Assets/Editor/GravityManagerEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(GravityManager))]
+public class GravityManagerEditor : Editor
+{
+    public int activeItemIndex = -1;
+
+    public override void OnInspectorGUI()
+    {
+        GravityManager _target = target as GravityManager;
+
+        base.OnInspectorGUI();
+        GUILayout.Label("Gravity Item Creation");
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Add"))
+        {
+            _target.activeIndex = _target.AddPoint();
+        }
+        GUILayout.Button("Delete");
+
+        GUILayout.EndHorizontal();
+
+
+        //GUILayout.BeginHorizontal();
+        GUILayout.Label("Active Item:");
+        _target.activeIndex = (int)GUILayout.HorizontalSlider(_target.activeIndex, 0, _target.points.Count);
+        //GUILayout.EndHorizontal();
+        
+    }
+}
---
+++ Assets/Scripts/GravityManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GravityManager : MonoBehaviour
+{
+    [HideInInspector]
+    public List<Vector3> points;
+    [HideInInspector]
+    public int activeIndex = -1;
+
+    private const int drawPointsCount = 3;
+
+    /// <summary>
+    /// Debugging information should be put here.
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        if (points.Count == 0)
+            return;
+
+        if (activeIndex < 0)
+            return;
+
+        int startDraw = activeIndex - drawPointsCount;
+        if (startDraw < 0)
+            startDraw = 0;
+        int endDraw = activeIndex + drawPointsCount;
+        if (endDraw > points.Count - 1)
+            endDraw = points.Count - 1;
+
+        Gizmos.color = Color.blue;
+        for (int i = startDraw; i <= endDraw; i++)
+            Gizmos.DrawSphere(points[i], 1);
+    }
+
+    public int AddPoint()
+    {
+        Vector3 newPoint;
+        if (points.Count > 0)
+        {
+            newPoint = points[points.Count - 1];
+            newPoint = new Vector3(newPoint.x, newPoint.y, newPoint.z);
+        }
+        else
+            newPoint = new Vector3(0, 0, 0);
+
+        points.Add(newPoint);
+
+        return points.IndexOf(newPoint);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
